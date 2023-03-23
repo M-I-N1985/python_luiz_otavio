bdb0b2375cdd750828756907daacc78235af6060
Iuri Nunes <iuri.mnunes@gmail.com>
2023-03-22 23:13:55 -0300

    envio de novas aulas

---
+++ aula273_if__name__main__.py
@@ -0,0 +1,2 @@
+from modulo_ import soma
+
---
+++ modulo_.py
@@ -0,0 +1,9 @@
+def soma(x: float, y: float) -> float:
+    return x + y
+
+
+#if __name__ == "__main__":
+#    print(soma(10, 20))
+
+print(__name__)
+print(soma(10, 20))
