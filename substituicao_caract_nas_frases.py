452f6eb9d87830a1f48df1b8342c3ae67fc08e3a
Iuri Nunes <iuri.mnunes@gmail.com>
2023-03-29 23:49:16 -0300

    envio de novas aulas

---
+++ substituicao_caract_nas_frases.py
@@ -0,0 +1,51 @@
+
+frase = input("Digite a frase: ")
+# frase = "3. Inção às"
+frase = frase.lower().split()
+nova_frase = ""
+nova_palavra = ""
+
+for palavra in frase:
+    for letra in palavra:
+        if letra in ['á', 'à', 'ã', 'â']:
+            letra = 'a'
+            nova_palavra = nova_palavra + letra
+        elif letra in ['é', 'ê']:
+            letra = 'e'
+            nova_palavra = nova_palavra + letra
+        elif letra == 'í':
+            letra = 'i'
+            nova_palavra = nova_palavra + letra
+        elif letra in ['ó', 'ô']:
+            letra = 'o'
+            nova_palavra = nova_palavra + letra
+        elif letra == 'ú':
+            letra = 'u'
+            nova_palavra = nova_palavra + letra
+        elif letra == 'ç':
+            letra = 'c'
+            nova_palavra = nova_palavra + letra
+        elif letra in "(":
+            letra = '_'
+            nova_palavra = nova_palavra + letra
+        elif letra in ')':
+            letra = '_'
+            nova_palavra = nova_palavra + letra
+        elif letra in ' ':
+            letra = '_'
+            nova_palavra = nova_palavra + letra
+        elif letra in '.':
+            letra = '_'
+            nova_palavra = nova_palavra + letra
+        elif letra in ",":
+            letra = '_'
+            nova_palavra = nova_palavra + letra
+
+        else:
+            nova_palavra = nova_palavra + letra
+
+    nova_palavra = nova_palavra + "_"
+
+nova_frase = 'aula' + nova_palavra
+nova_frase = nova_frase[:-1] + ".py"
+print(nova_frase)
